cf04f57e3301011afe103f86210dc47dcd3c3853
Carlos Ivan Alavez Estrada <carlosalavez@tada.mx>
2023-12-05 13:31:17 -0600

    primera etapa formulacion conceptos

--- TadaNomina/Models/ClassCore/CalculoNomina/ClassProcesosNomina.cs
+++ TadaNomina/Models/ClassCore/CalculoNomina/ClassProcesosNomina.cs
@@ -69,6 +69,7 @@ namespace TadaNomina.Models.ClassCore.CalculoNomina
         internal string[] _tipoEsquemaR = { "Tradicional", "Esquema", "Mixto" };
         internal ConfiguracionFechasCalculos configuracionFechas;
         internal List<ModelDiasTrabajadosAguinaldo> ListDiasTrabajadosAguinaldo;
+        internal List<vConceptos> conceptosNominaFormula;
 
         internal bool AjusteSecundario = false;
         internal bool AjusteAnual = false;
@@ -154,6 +155,7 @@ namespace TadaNomina.Models.ClassCore.CalculoNomina
             ProcesaIncidenciasProgramadas(Periodo.DescuentosFijos, Periodo.IdUnidadNegocio, IdPeriodo, IdEmpleado, IdUsuario);
             GetDatosUnidadNegocio(Periodo.IdUnidadNegocio);
             GetListEmpleados(UnidadNegocio.IdUnidadNegocio);
+            GetListConceptosNominaFormula(UnidadNegocio.IdCliente);
 
             if (Periodo.TipoNomina == "Nomina")
             {
@@ -261,6 +263,14 @@ namespace TadaNomina.Models.ClassCore.CalculoNomina
                 GetSubsidioSAT(4, Periodo.FechaFin);
                 GetListEmpleadosPTU(Periodo.IdPeriodoNomina);
             }                       
+        }
+
+        private void GetListConceptosNominaFormula(int idCliente)
+        {
+            using (TadaNominaEntities entidad = new TadaNominaEntities())
+            {
+                conceptosNominaFormula = entidad.vConceptos.Where(x => x.IdCliente == idCliente && x.IdEstatus == 1 && x.Formula != string.Empty).ToList();
+            }
         }
 
         public void getSaldos(int IdUnidadNegocio, DateTime FechaInicio, DateTime FechaFin)
