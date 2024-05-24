b476df2f97499a48965152d6915413f6973a8375
Carlos Ivan Alavez Estrada <char_ing15@hotmail.com>
2024-05-23 18:01:20 -0600

    ajuste subsidio

--- TadaNomina/Models/ClassCore/CalculoNomina/ClassCalculoImpuestos.cs
+++ TadaNomina/Models/ClassCore/CalculoNomina/ClassCalculoImpuestos.cs
@@ -497,10 +497,12 @@ namespace TadaNomina.Models.ClassCore.CalculoNomina
                 if ((ajuste ?? false))
                 {
                     var queryAjuste = (from b in ListNominaAjuste.Where(b => b.IdEmpleado == IdEmpleado) select b.Subsidio).Sum();
-                    nominaTrabajo.Subsidio += queryAjuste;
-
-                    if(nominaTrabajo.Subsidio <= valorTopeMensual && nominaTrabajo.BaseGravada <= topeParaSubsidio)
-                        nominaTrabajo.Subsidio = valorTopeMensual - queryAjuste;
+                    if (nominaTrabajo.Subsidio <= valorTopeMensual && nominaTrabajo.BaseGravada <= topeParaSubsidio)
+                    {
+                        var queryAjuste1 = (from b in ListNominaAjuste.Where(b => b.IdEmpleado == IdEmpleado) select b.Subsidio).Sum();
+                        nominaTrabajo.Subsidio += queryAjuste1;
+                        nominaTrabajo.Subsidio = valorTopeMensual - queryAjuste1;
+                    }
                 }
             }
         }
